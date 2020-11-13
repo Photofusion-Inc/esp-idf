cd62ffca7901c6ebaafbf41b8a1641d5860d4246
ryan-lastufka <ryan.lastufka@gmail.com>
2020-11-13 10:13:47 -0600

    wip

--- conanfile.py
+++ conanfile.py
@@ -17,31 +17,26 @@ class EspIdfConan(ConanFile):
     def source(self):
         self.run("git clone --recursive https://github.com/Photofusion-Inc/esp-idf.git")
         osName = platform.system()
-        print("SYSTEM: " + osName)
-        if(osName == 'Windows'):
-            print("EXECUTING ESP-IDF/INSTALL BAT IN: " + self.source_folder)
-            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "install.bat")
-            print("EXECUTING ESP-IDF/EXPORT BAT IN: " + self.source_folder)
-            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "export.bat")
-        elif(osName == 'Linux' or osName == 'Darwin'): # linux or mac
-            print("EXECUTING ESP-IDF/INSTALL BAT IN: " + self.source_folder)
-            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "install.sh")
-            print("EXECUTING ESP-IDF/EXPORT BAT IN: " + self.source_folder)
-            self.run(". " + self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "export.sh")
-
-
+        print("SOURCE: SYSTEM: " + osName)
+        self.idf_install(osName)
+        # self.idf_export(osName)
+        self.run("setx CC \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat\"")
+        # self.run("setx CFLAGS \"\"")
+        self.run("setx CXX \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat\"")
+        # self.run("setx CXXFLAGS \"-std=c++14\"")
         
+        self.run("setx ASM \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat\"")
+        self.run("cd /D " + self.source_folder + os.sep + "esp-idf" + " & ." + os.sep + "export.bat & idf.py set-target esp32s2")
 
-    # def build(self):
-        # cmake = CMake(self)
-        # cmake.configure(source_folder="hello")
-        # cmake.build()
+    def build(self):
+        osName = platform.system()
+        print("BUILD: SYSTEM: " + osName)
+        # RUN EXPORT (every time, yes)
+        self.idf_export(osName)
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
+        # GO TO DIRECTORY
+        # idf.py build
+        self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "export.bat & idf.py build")
 
     # def package(self):
 
@@ -49,4 +44,20 @@ class EspIdfConan(ConanFile):
 
     # def package_info(self):
         # self.cpp_info.libs = ["hello"]
+
+    def idf_install(self, osName):
+        if(osName == 'Windows'):
+            print("EXECUTING ESP-IDF/INSTALL BAT IN: " + self.source_folder)
+            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "install.bat")
+        elif(osName == 'Linux' or osName == 'Darwin'): # linux or mac
+            print("EXECUTING ESP-IDF/INSTALL SH IN: " + self.source_folder)
+            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "install.sh")
+
+    def idf_export(self, osName):
+        if(osName == 'Windows'):
+            print("EXECUTING ESP-IDF/EXPORT BAT IN: " + self.source_folder)
+            self.run(self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "export.bat")
+        elif(osName == 'Linux' or osName == 'Darwin'): # linux or mac
+            print("EXECUTING ESP-IDF/EXPORT SH IN: " + self.source_folder)
+            self.run(". " + self.source_folder + os.sep + "esp-idf" + os.sep + "." + os.sep + "export.sh")
 
